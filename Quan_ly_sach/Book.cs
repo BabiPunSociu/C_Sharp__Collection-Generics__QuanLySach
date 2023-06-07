b1c7974b36ce10bee771c1445697ea9a5b5d98f3
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:05:53 +0700

    DungNguyen

---
+++ Quan_ly_sach/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_ly_sach
+{
+    internal class Book
+    {
+        int id;
+        string title, author, publisher;    // Tên sách, Tác giả, Nhà xuất bản
+        int year;
+        public double price;
+        public Book(int id, string title, string author, string publisher, int year, double price)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.year = year;
+            this.price = price;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Mã sách: " + id);
+            Console.WriteLine("Tên sách: " + title);
+            Console.WriteLine("Tên tác giả: " + author);
+            Console.WriteLine("Nhà xuất bản: " + publisher);
+            Console.WriteLine("Năm xuất bản: " + year);
+            Console.WriteLine("Giá bán: " + price);
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+}
---
+++ Quan_ly_sach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_ly_sach
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book(1, "Lap trinh C", "Nguyen Van A", "DH GTVT", 2010, 150),
+                new Book(2, "Cha giau cha ngheo", "Nguyen Van B", "Kim Dong", 2002, 250),
+                new Book(3, "Dac Nhan Tam", "Dale Canagine", "Kim Dong", 2001, 300),
+                new Book(4, "Hoc ve tien", "Nguoi Nhat Ban", "Kim Dong", 2009, 123),
+                new Book(5, "Doi ngan dung ngu dai", "Napoleong Hill", "NXB Tre", 2015, 315),
+            };
+            
+            // in danh sach
+            foreach(Book book in books)
+            {
+                book.display();
+            }
+
+            // sắp xếp danh sách theo giá tăng dần:
+            books = books.OrderBy(b => b.price).ToList();   //  Linq
+            // in danh sach
+            foreach (Book book in books)
+            {
+                book.display();
+            }
+        }
+    }
+}
